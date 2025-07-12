5d3a43d4ef0fca8350baca8801e115a1141c3ade
Akingram <guilland.killian@gmail.com>
2025-07-12 18:13:56 +0200

    fix: conflicting routes

--- app/routes/$.tsx
+++
@@ -1,9 +0,0 @@
-import { Heading } from '@/components/atoms/heading';
-
-export default function NotFound({ children }: { children: React.ReactNode }) {
-    return (
-        <div className="size-full flex items-center justify-center">
-            <Heading>404 | Cette page n'existe pas</Heading>
-        </div>
-    );
-}
